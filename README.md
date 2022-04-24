bccd6357f4f82c4b4980bf922f1b9cdb36ab9c30
刘善平 <1319767110@qq.com>
2022-04-24 15:05:17 +0800

    wolai alfred 初版代码

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
-# wolai-alfred
-wolai alfred 搜索插件
+# Wolai Alfred 搜索插件
+
+1. 因个人能力有限,搞JAVA的,不熟悉Python和Alfred,此版本目前只支持网页版本,不支持客户端版本,后续可能会支持,并维护添加新功能
+2. wolai的history似乎是本地缓存的查询,不涉及到HTTP请求,所以浏览历史暂时也没想到咋整
+3. 如有问题,请GitHub提交issue(请勿提交百度上就能找到的问题,浪费大家时间,当然给钱的话另说,咨询一次10),
+4. 因为本人非Python使用者,写的很简单,也可能在大佬看来也很烂,欢迎大佬搞PR,甚至可以在issue上提交自己写的,我也会贴到README里面,给你引流
+
+## 系统变量
+- wolaiSpaceId
+- cookie
+- userId
+- userName
+
+## 食用方法
+0. 如直接输入https://www.wolai.com/不能进入个人页面,则先登录wolai
+1. 打开浏览器F12开启调试台,并切换至[网络],建议使用Firefox,直接中文,Chrome默认英文
+2. 输入网址: https://www.wolai.com/
+3. 在请求筛选框内输入"getUserInfo",找到对应的请求,并单击
+4. 在[标头]处找到[请求标头],将其中的[Cookie]里面的内容复制下来
+    格式类似:"token=I6IkpXVCJ9.eyJ1Td9.mzCb7fA; _gid=GA1; _gat_UA-..."
+5. 将其复制下来,并粘贴到Alfred的cookie参数里面
+   ![img.png](./img/img.png)
+   ![img_2.png](./img/img_2.png)
+6. 返回F12调试台,找到响应,点击"{}"将数据格式化,然后从中找到[userId],[userName],并参照第5步将参数填入Alfred
+   ![img_1.png](./img/img_1.png)
+7. 重复第3步,将输入框内容转换为"getWorkspaceData",如未找到,直接刷新网页,参照第6步找到[workspaces]下的[id],格式类似:"rjQnUKagSwDQx1MG",并填入Alfred
+8. 在Alfred输入[wolai 关键字], 如[wolai issue],即可以默认浏览器跳转到对应网页
+    效果如图:![img_3.png](./img/img_3.png)
+  
+## 下载地址
+   见上面源码中的 Wolai.alfredworkflow
+## 恰饭
+   虽然写的很简单,但我还是有脸皮恰饭,愿意打赏的给个1~5块,不要多给,我不配
+![img_4.png](./img/img_4.png) ![img_5.png](./img/img_5.png)
+
+
+
---
+++ src/wolai.py
@@ -0,0 +1,99 @@
+# -*- coding: UTF-8 -*-
+import gzip
+import http.client
+import json
+import os.path
+import sys
+from io import BytesIO
+from payload import Payload
+
+# 参数配置
+wolaiSpaceId = os.environ['wolaiSpaceId']
+cookie = os.environ['cookie']
+userId = os.environ['userId']
+userName = os.environ['userName']
+useDesktopClient = False
+url = "https://www.wolai.com/"
+clientUrl = "wolai://" + url
+
+# query参数
+alfredQuery = str(sys.argv[1])
+
+if (useDesktopClient == 'true') | (useDesktopClient == 'True') | (useDesktopClient == 'TRUE'):
+    useDesktopClient = True
+else:
+    useDesktopClient = False
+
+
+def buildWolaiSearchData():
+    query = {
+        "query": alfredQuery,
+        "workspaceId": wolaiSpaceId,
+        "fuzzy": "true",
+        "order": "desc",
+        "active": "1",
+        "pageId": ""
+    }
+
+    jsonData = json.dumps(query)
+    return jsonData
+
+
+def getWolaiurl():
+    if useDesktopClient:
+        return clientUrl + userName + "/"
+    else:
+        return url + userName + "/"
+
+
+searchResultList = []
+if alfredQuery and alfredQuery.strip():
+    headers = {"Content-type": "application/json", "Cookie": cookie}
+    conn = http.client.HTTPSConnection("api.wolai.com")
+    conn.request("POST", "/v1/search/advancedSearch", buildWolaiSearchData(), headers)
+    response = conn.getresponse()
+    buff = BytesIO(response.read())
+    data = gzip.GzipFile(fileobj=buff).read().decode("utf-8")
+
+    dataStr = json.dumps(data).replace("<em>", "").replace("</em>", "")
+    data = json.loads(dataStr)
+    conn.close()
+
+    searchResults = Payload(data)
+    try:
+        result = searchResults.data.get('result').get("items");
+        for x in result:
+            clientLink = getWolaiurl() + x.get("id");
+            link = getWolaiurl() + x.get("id");
+            x["clientLink"] = clientLink
+            x["webLink"] = url + x.get("id")
+            x["link"] = link
+            searchResultList.append(x)
+    except:
+        pass
+
+itemList = []
+for searchResultObject in searchResultList:
+    item = {
+        "type": searchResultObject.get("type"),
+        "title": searchResultObject.get("title"),
+        "arg": searchResultObject.get("link"),
+        "subtitle": ""
+    }
+    if searchResultObject.get("icon").__contains__("emoji"):
+        item["icon"] = searchResultObject.get("icon")[1]
+        item["autocomplete"] = searchResultObject.get("title")
+        itemList.append(item)
+
+items = {}
+if not itemList:
+    item = {
+        "uid": 1,
+        "type": "default",
+        "title": "Open Wolai - No results, empty query, or error",
+        "arg": getWolaiurl()
+    }
+    itemList.append(item)
+items["items"] = itemList
+items_json = json.dumps(items)
+sys.stdout.write(items_json)
